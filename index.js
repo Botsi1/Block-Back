1bae8d11d9a5531ef0143ad44324d8edf7000e45
Bautista Moreiro <bmoreiro1@gmail.com>
2023-03-06 16:39:31 +0000

    Update user.js and index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import multer from "multer";
 
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb("../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload",upload.single("file"), function(req,res){
+    const file = req.file
+    res.status(200).json(file.filename)
+})
+
+app.listen(3001,function(){
+    console.log("Conected")
+})
